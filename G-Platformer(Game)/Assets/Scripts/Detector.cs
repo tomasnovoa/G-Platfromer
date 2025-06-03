f63b73b3346742000d25f3dbcb9fc911e3fabaa0
tomasnovoa <dioshydrox@gmail.com>
2025-06-03 16:53:57 -0400

    aay shi

---
+++ G-Platformer(Game)/Assets/Scripts/Detector.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Events;
+public class Detector : MonoBehaviour
+{
+
+    public UnityEvent action;
+
+
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        action.Invoke();
+    }
+}
