e720569a28cf37c30be7400873a1119bc6a5a1d5
tomasnovoa <dioshydrox@gmail.com>
2025-06-24 16:46:28 -0400

    cosas cosas

---
+++ G-Platformer(Game)/Assets/Scripts/LaunchPad.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class LaunchPad : MonoBehaviour
+{
+    public float fuerza = 10f;
+    public Animator animator;
+   
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") )
+        {
+            Rigidbody2D rb2d = collision.GetComponent<Rigidbody2D>();
+            if (rb2d != null)
+            {
+                rb2d.linearVelocity = new Vector2(rb2d.linearVelocityX, 0f);
+                rb2d.AddForce(Vector2.up * fuerza, ForceMode2D.Impulse);
+                animator.Play("Jumper");
+                
+            }
+        }
+    }
+
+  
+
+    
+
+   
+
+
+}
