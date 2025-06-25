55f21aadff500ab121c629e47d0bbbfb48ffb54f
tomasnovoa <dioshydrox@gmail.com>
2025-06-25 17:48:50 -0400

    miau

--- G-Platformer(Game)/Assets/Scripts/Movement.cs
+++ G-Platformer(Game)/Assets/Scripts/Movement.cs
@@ -5,16 +5,17 @@ public class Movement : MonoBehaviour
 {
     public bool canMove;
     public Animator animator;
- 
+
     public float velocidadMovimiento;
     public float fuerzaSalto;
+    private float currentJumpForce;
     public Transform puntoSuelo;
     public float radioDeteccion = 0.2f;
     public LayerMask capaSuelo;
 
     private Rigidbody2D rb;
     private bool estaEnSuelo;
-  
+
     private bool preparandoSalto;
 
     void Start()
@@ -28,33 +29,33 @@ public class Movement : MonoBehaviour
         Move();
         Detector();
         Jump();
-        
+
     }
     void Move()
     {
         float direccion = 0f;
         if (Input.GetKey(KeyCode.A) && canMove)
         {
-            animator.SetBool("InMove",true);
+            animator.SetBool("InMove", true);
             direccion = -1f;
         }
         else if (Input.GetKey(KeyCode.D) && canMove)
         {
-            animator.SetBool("InMove",true);
+            animator.SetBool("InMove", true);
             direccion = 1f;
         }
         else if (!canMove)
         {
-            direccion=0f;
+            direccion = 0f;
         }
 
         rb.linearVelocity = new Vector2(direccion * velocidadMovimiento, rb.linearVelocityY);
 
 
-      if(rb.linearVelocity.magnitude == 0f)
-      {
-        animator.SetBool("InMove",false);
-      }
+        if (rb.linearVelocity.magnitude == 0f)
+        {
+            animator.SetBool("InMove", false);
+        }
 
     }
     void Detector()
@@ -73,11 +74,11 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump") && estaEnSuelo && canMove)
         {
-           
+
             animator.SetBool("HoldJump", true);
 
-            
- 
+
+
             preparandoSalto = true;
         }
 
@@ -85,11 +86,16 @@ public class Movement : MonoBehaviour
         {
             animator.SetTrigger("InJump");
             animator.SetBool("HoldJump", false);
-            rb.linearVelocity = new Vector2(rb.linearVelocityY, fuerzaSalto);
+            float value = fuerzaSalto * currentJumpForce;
+            rb.linearVelocity = new Vector2(rb.linearVelocityY, value);
             preparandoSalto = false;
         }
     }
- 
+
+    public void holdJump(float valor)
+    {
+        currentJumpForce = valor;
+    }
 }
 
 
